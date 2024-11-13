c436eca9c90d36ecf47320d14040d71f6e2d992a
Anthony Candaele <info@anthonycandaele.com>
2024-11-13 09:33:45 +0100

    implement navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#242222',
+				action: '#FA1600'
+			}
+		},
 	},
 	plugins: [],
 }
